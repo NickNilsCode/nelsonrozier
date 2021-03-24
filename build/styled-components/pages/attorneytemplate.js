15e3a4ebb1732fc0fa8d9aca49dc75d47fc84846
Nils Castillo <nils.castillo10@gmail.com>
2021-03-24 13:19:58 -0600

    build project and removed env variables

--- build/styled-components/pages/attorneytemplate.js
+++ build/styled-components/pages/attorneytemplate.js
@@ -7,20 +7,12 @@ exports.AttorneyDiv = void 0;
 
 var _styledComponents = _interopRequireDefault(require("styled-components"));
 
+var _templateObject;
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n    width: 100%;\n    position: relative;\n    img {\n      display: inherit;\n    }\n    @media (min-width: 900px){\n      img {\n        position: absolute;\n        top: 0;\n        right: 0;\n      }\n    }\n  "]);
-
-  _templateObject = function _templateObject() {
-    return data;
-  };
-
-  return data;
-}
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var AttorneyDiv = _styledComponents["default"].div(_templateObject());
+var AttorneyDiv = _styledComponents["default"].div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n    width: 100%;\n    position: relative;\n    img {\n      display: inherit;\n    }\n    @media (min-width: 900px){\n      img {\n        position: absolute;\n        top: 0;\n        right: 0;\n      }\n    }\n  "])));
 
 exports.AttorneyDiv = AttorneyDiv;
