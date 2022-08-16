bbe38eb97ee0c5a379f2f3e25a72f0429b295d79
Jitin Bhatt <109499326+seoagencyanalytics@users.noreply.github.com>
2022-08-16 09:47:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -160,7 +160,7 @@ app.get('/blog', (req, res) => {
     month: ""
   };
   res.set('Cache-Control', 'public, max-age=31557600');
-  res.send(returnHTML(data, blogBundle, BlogRoot, "Read Our Latest Blog About Personal Injury Visalia | Nelson Rozier & Christenson", "", ""));
+  res.send(returnHTML(data, blogBundle, BlogRoot, "Blog | Nelson Rozier & Christenson", "", ""));
 });
 app.get('/blogcattemplate', (req, res) => {
   let data = "";
